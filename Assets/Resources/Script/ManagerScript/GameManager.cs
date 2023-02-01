51ac00b39cbd22dfcb53c8d45e8cc1003f924de2
KimJeongGon <niceman1994@naver.com>
2023-02-01 15:51:23 +0900

    2023/02/01-2DGame_1

---
+++ Assets/Resources/Script/ManagerScript/GameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : ManagerSingleton<GameManager>
+{
+    public GameObject PresentsCanvas;
+    public GameObject GameNameCanvas;
+    public GameObject LogoCanvas;
+    public GameObject CoinCanvas;
+}
---
+++ Assets/Resources/Script/ManagerScript/ManagerSingleton2.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerSingleton2<T> : MonoBehaviour where T : MonoBehaviour
+{
+	private static object _Lock = new object();
+	private static T instance;
+
+	public static T Instance
+	{
+		get
+		{
+			lock (_Lock)
+			{
+				if (instance == null)
+				{
+					GameObject obj;
+					obj = GameObject.Find(typeof(T).Name);
+
+					if (obj == null)
+					{
+						obj = new GameObject(typeof(T).Name);
+						instance = obj.AddComponent<T>();
+					}
+					else
+					{
+						instance = obj.GetComponent<T>();
+					}
+				}
+				return instance;
+			}
+		}
+	}
+}
