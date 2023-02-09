f95dbef013d1532d75ee3e1b32e493e7745e93f6
niceman1994 <niceman1994@naver.com>
2023-02-10 01:25:01 +0900

    2023/02/10-2DGame_1

--- Assets/Resources/Script/ScrollBackground.cs
+++ Assets/Resources/Script/ScrollBackground.cs
@@ -35,7 +35,7 @@ public class ScrollBackground : MonoBehaviour
 				scrollBackground[i].gameObject.transform.position = new Vector3(
 					scrollBackground[i].gameObject.transform.position.x + ((-BackgroundManager.Instance.Speed - 0.05f) * Time.deltaTime), 0.0f, 0.0f);
 		
-				if (scrollBackground[i].gameObject.transform.position.x < leftPosX[i] * 2.897f) // 움직이는 배경이 카메라에서 완전히 벗어났을 때의 x 좌표 : -115.84
+				if (scrollBackground[i].gameObject.transform.position.x < leftPosX[i] * 2.91f) // 움직이는 배경이 카메라에서 완전히 벗어났을 때의 x 좌표 : -115.84
 				{
 					Vector3 nextPos = scrollBackground[i].gameObject.transform.position;
 					nextPos = new Vector3(nextPos.x + rightPosX[i] + 38.3993f, nextPos.y, nextPos.z);
